8077a2f9ec159e680f25897e19b32ad38c0a7bca
Moses Ojiko <mosesojiko999@gmail.com>
2021-07-17 09:35:17 -0700

    week7 assignment done

---
+++ models/User.js
@@ -0,0 +1,46 @@
+let user = [
+    {
+        id: 1,
+        email: "mosesojiko@gmail.com",
+        fullName: "Moses Ojiko",
+        password: "123456789"
+    },
+
+    {
+        id: 2,
+        email: "johnsmith@gmail.com",
+        fullName: "John Smith",
+        password: "913247853"
+    },
+
+    {
+        id: 3,
+        email: "frankedward@gmail.com",
+        fullName: "Frank Edward",
+        password: "235432654"
+    },
+
+    {
+        id: 4,
+        email: "adasenach@gmail.com",
+        fullName: "Ada Senach",
+        password: "444355243"
+    },
+
+    {
+        id: 5,
+        email: "aylive@gmail.com",
+        fullName: "Ay Live",
+        password: "3355224422"
+    },
+
+    {
+        id: 6,
+        email: "basketmouth@gmail.com",
+        fullName: "Basket Mouth",
+        password: "542234263"
+    }
+
+];
+
+module.exports = user;
